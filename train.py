8e61f860431782a637eebe73790f4420b4f41b56
dstch <dstch@163.com>
2018-11-24 18:19:47 +0800

    modify train code

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from bert_master import modeling, tokenization, optimization
-import os, collections
+import os, collections, csv
 
 
 class InputFeatures(object):
@@ -13,34 +13,57 @@ class InputFeatures(object):
         self.label_id = label_id
 
 
+class InputExample(object):
+    """A single training/test example for simple sequence classification."""
+
+    def __init__(self, guid, text_a, text_b=None, label=None):
+        """Constructs a InputExample.
+
+        Args:
+          guid: Unique id for the example.
+          text_a: string. The untokenized text of the first sequence. For single
+            sequence tasks, only this sequence must be specified.
+          text_b: (Optional) string. The untokenized text of the second sequence.
+            Only must be specified for sequence pair tasks.
+          label: (Optional) string. The label of the example. This should be
+            specified for train and dev examples, but not for test examples.
+        """
+        self.guid = guid
+        self.text_a = text_a
+        self.text_b = text_b
+        self.label = label
+
+
 class train():
     def __init__(self):
         flags = tf.flags
         self.FLAGS = flags.FLAGS
         ## Required parameters
         flags.DEFINE_string(
-            "data_dir", None,
+            "data_dir", r'E:\github\Quora_question_classification_ExCalibur\train_data',
             "The input data dir. Should contain the .tsv files (or other data files) "
             "for the task.")
 
         flags.DEFINE_string(
-            "bert_config_file", None,
+            "bert_config_file",
+            r'E:\github\Quora_question_classification_ExCalibur\raw_data\bert_model\bert_config.json',
             "The config json file corresponding to the pre-trained BERT model. "
             "This specifies the model architecture.")
 
-        flags.DEFINE_string("task_name", None, "The name of the task to train.")
+        flags.DEFINE_string("task_name", 'Quora', "The name of the task to train.")
 
-        flags.DEFINE_string("vocab_file", None,
+        flags.DEFINE_string("vocab_file",
+                            r'E:\github\Quora_question_classification_ExCalibur\raw_data\bert_model\vocab.txt',
                             "The vocabulary file that the BERT model was trained on.")
 
         flags.DEFINE_string(
-            "output_dir", None,
+            "output_dir", r'E:\github\Quora_question_classification_ExCalibur\train_model',
             "The output directory where the model checkpoints will be written.")
 
         ## Other parameters
 
         flags.DEFINE_string(
-            "init_checkpoint", None,
+            "init_checkpoint", r'E:\github\Quora_question_classification_ExCalibur\raw_data\bert_model\bert_model.ckpt',
             "Initial checkpoint (usually from a pre-trained BERT model).")
 
         flags.DEFINE_bool(
@@ -54,9 +77,9 @@ class train():
             "Sequences longer than this will be truncated, and sequences shorter "
             "than this will be padded.")
 
-        flags.DEFINE_bool("do_train", False, "Whether to run training.")
+        flags.DEFINE_bool("do_train", True, "Whether to run training.")
 
-        flags.DEFINE_bool("do_eval", False, "Whether to run eval on the dev set.")
+        flags.DEFINE_bool("do_eval", True, "Whether to run eval on the dev set.")
 
         flags.DEFINE_bool(
             "do_predict", False,
@@ -512,7 +535,41 @@ class train():
                 writer.write("%s = %s\n" % (key, str(result[key])))
 
     def get_train_examples(self, data_dir):
-        pass
+        return self._create_examples(
+            self._read_csv(os.path.join(data_dir, "train.csv")), "train")
+
+    def _read_csv(self, input_file, quotechar=None):
+        """Reads a tab separated value file."""
+        with tf.gfile.Open(input_file, "r") as f:
+            reader = csv.reader(f, delimiter=",", quotechar=quotechar)
+            lines = []
+            for line in reader:
+                lines.append(line)
+            return lines
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
+            guid = "%s-%s" % (set_type, tokenization.convert_to_unicode(line[0]))
+            text_a = tokenization.convert_to_unicode(line[1])
+            # text_b = tokenization.convert_to_unicode(line[9])
+            if set_type == "test":
+                label = "contradiction"
+            else:
+                label = tokenization.convert_to_unicode(line[-1])
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, label=label))
+        return examples
 
     def get_dev_examples(self, data_dir):
-        pass
+        return self._create_examples(
+            self._read_csv(os.path.join(data_dir, "dev.csv")),
+            "dev_matched")
+
+
+if __name__ == '__main__':
+    train_helper = train()
+    train_helper.main()
