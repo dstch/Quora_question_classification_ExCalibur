4a1d8a8971f3402eaadb34219d25cf59de640e73
dstch <dstch@163.com>
2018-12-01 13:27:12 +0800

    complete embedding code

--- Bi-LSTM/embedding.py
+++ Bi-LSTM/embedding.py
@@ -6,8 +6,21 @@ class embbedding():
     def __init__(self):
         self.model_file = 'glove.840B.300d/glove.840B.300d.txt'
 
-    def creat_vector_data(self, csv_file):
+    def creat_vector_data(self, csv_file, save_csv_file):
         # load model
         model = gensim.models.KeyedVectors.load_word2vec_format(self.model_file)
         data = pd.read_csv(csv_file)
-        for sentence in data['question_text']
+        vectors = []
+        for sentence in data['question_text']:
+            for index, word in enumerate(sentence.split()):
+                if index == 0:
+                    vector = model[word]
+                else:
+                    vector += model[word]
+            vectors.append(vector / len(sentence.split()))
+        id_df = data[['qid', 'target']]
+        vector_df = pd.DataFrame(vectors, columns=['question_text'])
+        result = id_df.join(vector_df)
+        result.to_csv(save_csv_file, index=False)
+
+
