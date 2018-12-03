d32e2fc556d4e2d7c0024460e5304ec48cc8eaea
dstch <dstch@163.com>
2018-12-03 20:30:37 +0800

    complete modify bi_lstm model code

--- Bi-LSTM/bi_lstm_model.py
+++ Bi-LSTM/bi_lstm_model.py
@@ -14,6 +14,8 @@ class bi_lstm():
         lstm_bw_cell = tf.nn.rnn_cell.BasicLSTMCell(FLAGS.n_hidden)
         lstm_bw_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_bw_cell, output_keep_prob=0.7)
 
-        outputs, _, _ = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, input_data, dtype=tf.float32)
-
+        outputs, _ = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, input_data, dtype=tf.float32)
+        # 双向LSTM，输出outputs为两个cell的output
+        # 将两个cell的outputs进行拼接
+        outputs = tf.concat(outputs, 2)
         return tf.matmul(outputs[-1], weights['out']) + biases['out']
