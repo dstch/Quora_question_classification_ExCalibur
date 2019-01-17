e51ffd811c0f44e3ca251cc75719324d1b676233
dstch <dstch@163.com>
2019-01-17 20:59:21 +0800

    new temp test code, new clean word code

--- Bi-LSTM/temp_test.py
+++ Bi-LSTM/temp_test.py
@@ -1,36 +1,22 @@
 import tensorflow as tf
 import operator
-from tqdm import tqdm
+
 import string
 import pandas as pd
 import numpy as np
 
-# parameters config
-flags = tf.flags
-FLAGS = flags.FLAGS
+import re
 
-flags.DEFINE_string("train_data_path", "../train_data/train.csv", "train data path")
-flags.DEFINE_string("test_data_path", "../train_data/test.csv", "test data path")
-flags.DEFINE_string("checkpoint_path", "./logs/checkpoint", "model save path")
-flags.DEFINE_string("glove_path", "./glove.840B.300d/glove.840B.300d.txt", "pre-train embedding model path")
-flags.DEFINE_integer("max_sentence_len", 30, "max length of sentence")
-flags.DEFINE_integer("embedding_dim", 300, "word embedding dim")
-flags.DEFINE_integer("n_hidden", 256, "LSTM hidden layer num of features")
-flags.DEFINE_integer("batch_size", 1000, "batch size")
-flags.DEFINE_integer("buffer_size", 1000, "batch size")
-flags.DEFINE_integer("n_classes", 2, "number of classes")
-flags.DEFINE_float("learning_rate", 0.001, "learnning rate")
-flags.DEFINE_float("epoch", 10, "epoch of train")
-flags.DEFINE_integer("attention_size", 256, "attention size")
+# 检查embedding
+train_df = pd.read_csv("../input/train.csv")
+test_df = pd.read_csv("../input/test.csv")
+df = pd.concat([train_df, test_df], sort=True)
 
 
-def build_vocab(sentences, verbose=True):
-    """
-    :param sentences: list of list of words
-    :return: dictionary of words and their count
-    """
+def build_vocab(texts):
+    sentences = texts.apply(lambda x: x.split()).values
     vocab = {}
-    for sentence in tqdm(sentences, disable=(not verbose)):
+    for sentence in sentences:
         for word in sentence:
             try:
                 vocab[word] += 1
@@ -39,114 +25,118 @@ def build_vocab(sentences, verbose=True):
     return vocab
 
 
-def check_coverage(vocab, embeddings_index):
-    a = {}
-    oov = {}
-    k = 0
-    i = 0
-    for word in tqdm(vocab):
-        try:
-            a[word] = embeddings_index[word]
-            k += vocab[word]
-        except:
-            oov[word] = vocab[word]
-            i += vocab[word]
-            pass
-    print('Found embeddings for {:.2%} of vocab'.format(len(a) / len(vocab)))
-    print('Found embeddings for  {:.2%} of all text'.format(k / (k + i)))
-    sorted_x = sorted(oov.items(), key=operator.itemgetter(1))[::-1]
-
-    return sorted_x
+puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=', '#', '*',
+          '+', '\\', '•', '~', '@', '£',
+          '·', '_', '{', '}', '©', '^', '®', '`', '<', '→', '°', '€', '™', '›', '♥', '←', '×', '§', '″', '′', 'Â', '█',
+          '½', 'à', '…',
+          '“', '★', '”', '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾', '═', '¦', '║', '―', '¥',
+          '▓', '—', '‹', '─',
+          '▒', '：', '¼', '⊕', '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲', 'è', '¸', '¾',
+          'Ã', '⋅', '‘', '∞',
+          '∙', '）', '↓', '、', '│', '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø', '¹',
+          '≤', '‡', '√', ]
 
 
-def clean_punctuation(sentence):
-    sentence = [x for x in sentence if x not in string.punctuation]
-    sentence = ''.join(sentence)
-    return sentence
+def clean_text(x):
+    x = str(x)
+    for punct in puncts:
+        x = x.replace(punct, f' {punct} ')
+    return x
 
 
-def model1(n_hidden, input_data, weights, biases):
-    lstm_fw_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden)
-    lstm_fw_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_fw_cell, output_keep_prob=0.7)
-    lstm_bw_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden)
-    lstm_bw_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_bw_cell, output_keep_prob=0.7)
-
-    outputs, _ = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, input_data, dtype=tf.float32)
-    # 双向LSTM，输出outputs为两个cell的output
-    # 将两个cell的outputs进行拼接
-    outputs = tf.concat(outputs, 2)
-    return tf.matmul(tf.transpose(outputs, [1, 0, 2])[-1], weights['out']) + biases['out']
+def clean_numbers(x):
+    x = re.sub('[0-9]{5,}', '#####', x)
+    x = re.sub('[0-9]{4}', '####', x)
+    x = re.sub('[0-9]{3}', '###', x)
+    x = re.sub('[0-9]{2}', '##', x)
+    return x
 
 
-def get_coefs(word, *arr):
-    return word, np.asarray(arr, dtype='float32')
+mispell_dict = {"ain't": "is not", "aren't": "are not", "can't": "cannot", "'cause": "because",
+                "could've": "could have", "couldn't": "could not", "didn't": "did not", "doesn't": "does not",
+                "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not", "he'd": "he would",
+                "he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will",
+                "how's": "how is", "I'd": "I would", "I'd've": "I would have", "I'll": "I will",
+                "I'll've": "I will have", "I'm": "I am", "I've": "I have", "i'd": "i would", "i'd've": "i would have",
+                "i'll": "i will", "i'll've": "i will have", "i'm": "i am", "i've": "i have", "isn't": "is not",
+                "it'd": "it would", "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have",
+                "it's": "it is", "let's": "let us", "ma'am": "madam", "mayn't": "may not", "might've": "might have",
+                "mightn't": "might not", "mightn't've": "might not have", "must've": "must have", "mustn't": "must not",
+                "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have",
+                "o'clock": "of the clock", "oughtn't": "ought not", "oughtn't've": "ought not have",
+                "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would",
+                "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is",
+                "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have",
+                "so've": "so have", "so's": "so as", "this's": "this is", "that'd": "that would",
+                "that'd've": "that would have", "that's": "that is", "there'd": "there would",
+                "there'd've": "there would have", "there's": "there is", "here's": "here is", "they'd": "they would",
+                "they'd've": "they would have", "they'll": "they will", "they'll've": "they will have",
+                "they're": "they are", "they've": "they have", "to've": "to have", "wasn't": "was not",
+                "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have",
+                "we're": "we are", "we've": "we have", "weren't": "were not", "what'll": "what will",
+                "what'll've": "what will have", "what're": "what are", "what's": "what is", "what've": "what have",
+                "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is",
+                "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is",
+                "who've": "who have", "why's": "why is", "why've": "why have", "will've": "will have",
+                "won't": "will not", "won't've": "will not have", "would've": "would have", "wouldn't": "would not",
+                "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would",
+                "y'all'd've": "you all would have", "y'all're": "you all are", "y'all've": "you all have",
+                "you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have",
+                "you're": "you are", "you've": "you have", 'colour': 'color', 'centre': 'center',
+                'favourite': 'favorite', 'travelling': 'traveling', 'counselling': 'counseling', 'theatre': 'theater',
+                'cancelled': 'canceled', 'labour': 'labor', 'organisation': 'organization', 'wwii': 'world war 2',
+                'citicise': 'criticize', 'youtu ': 'youtube ', 'Qoura': 'Quora', 'sallary': 'salary', 'Whta': 'What',
+                'narcisist': 'narcissist', 'howdo': 'how do', 'whatare': 'what are', 'howcan': 'how can',
+                'howmuch': 'how much', 'howmany': 'how many', 'whydo': 'why do', 'doI': 'do I', 'theBest': 'the best',
+                'howdoes': 'how does', 'mastrubation': 'masturbation', 'mastrubate': 'masturbate',
+                "mastrubating": 'masturbating', 'pennis': 'penis', 'Etherium': 'Ethereum', 'narcissit': 'narcissist',
+                'bigdata': 'big data', '2k17': '2017', '2k18': '2018', 'qouta': 'quota', 'exboyfriend': 'ex boyfriend',
+                'airhostess': 'air hostess', "whst": 'what', 'watsapp': 'whatsapp', 'demonitisation': 'demonetization',
+                'demonitization': 'demonetization', 'demonetisation': 'demonetization'}
 
 
-def model(n_hidden, input_data, weights, biases, attention_size):
-    # bi-lstm
-    lstm_fw_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden)
-    lstm_fw_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_fw_cell, output_keep_prob=0.7)
-    lstm_bw_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden)
-    lstm_bw_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_bw_cell, output_keep_prob=0.7)
-    outputs, _ = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, input_data, dtype=tf.float32)
-    outputs = tf.concat(outputs, 2)
-    outputs = tf.transpose(outputs, [1, 0, 2])
-    # attention layer
-    with tf.name_scope('attention'), tf.variable_scope('attention'):
-        attention_w = tf.Variable(tf.truncated_normal([2 * n_hidden, attention_size], stddev=0.1), name='attention_w')
-        attention_b = tf.Variable(tf.constant(0.1, shape=[attention_size]), name='attention_b')
-        u_list = []
-        for t in range(FLAGS.max_sentence_len):
-            u_t = tf.tanh(tf.matmul(outputs[t], attention_w) + attention_b)
-            u_list.append(u_t)
-        u_w = tf.Variable(tf.truncated_normal([attention_size, 1], stddev=0.1), name='attention_uw')
-        attn_z = []
-        for t in range(FLAGS.max_sentence_len):
-            z_t = tf.matmul(u_list[t], u_w)
-            attn_z.append(z_t)
-        # transform to batch_size * sequence_length
-        attn_zconcat = tf.concat(attn_z, axis=1)
-        alpha = tf.nn.softmax(attn_zconcat)
-        # transform to sequence_length * batch_size * 1 , same rank as outputs
-        alpha_trans = tf.reshape(tf.transpose(alpha, [1, 0]), [FLAGS.max_sentence_len, -1, 1])
-        final_output = tf.reduce_sum(outputs * alpha_trans, 0)
-
-    # 双向LSTM，输出outputs为两个cell的output
-    # 将两个cell的outputs进行拼接
-    # outputs = tf.concat(outputs, 2)
-    # return tf.matmul(tf.transpose(outputs, [1, 0, 2])[-1], weights['out']) + biases['out']
-    return tf.matmul(final_output, weights['out']) + biases['out']
+def _get_mispell(mispell_dict):
+    mispell_re = re.compile('(%s)' % '|'.join(mispell_dict.keys()))
+    return mispell_dict, mispell_re
 
 
-# input_data = tf.placeholder(dtype=tf.float32, shape=[None, FLAGS.max_sentence_len, FLAGS.embedding_dim],
-#                             name='input_data')
-#
-# # Define weights
-# weights = {
-#     # Hidden layer weights => 2*n_hidden because of foward + backward cells
-#     'out': tf.Variable(tf.random_normal([2 * FLAGS.n_hidden, FLAGS.n_classes]))
-# }
-# biases = {
-#     'out': tf.Variable(tf.random_normal([FLAGS.n_classes]))
-# }
-# pred = model(FLAGS.n_hidden, input_data, weights, biases, FLAGS.attention_size)
-# print(pred)
+mispellings, mispellings_re = _get_mispell(mispell_dict)
 
-# train_data = pd.read_csv(FLAGS.train_data_path)
-# # clean data
-# sentences = train_data["question_text"].map(lambda x: clean_punctuation(x))
-# vocab = build_vocab(sentences)
-# embeddings_index = dict(get_coefs(*o.split(" ")) for o in open(FLAGS.glove_path, encoding='utf-8'))
-# oov = check_coverage(vocab,embeddings_index)
 
-# global_step = tf.Variable(0, trainable=False)
-#
-# init = tf.group(tf.initialize_all_variables(), tf.local_variables_initializer())
-# with tf.Session() as sess:
-#     sess.run(init)
-#     for step in range(1, 5):
-#         assign_op = global_step.assign(step)
-#         print(sess.run())
+def replace_typical_misspell(text):
+    def replace(match):
+        return mispellings[match.group(0)]
 
-t1=tf.Variable(0, trainable=False)
-print(t1)
+    return mispellings_re.sub(replace, text)
+
+
+vocab = build_vocab(df['question_text'])
+
+sin = len(train_df[train_df["target"] == 0])
+insin = len(train_df[train_df["target"] == 1])
+persin = (sin / (sin + insin)) * 100
+perinsin = (insin / (sin + insin)) * 100
+print(
+    "# Sincere questions: {:,}({:.2f}%) and # Insincere questions: {:,}({:.2f}%)".format(sin, persin, insin, perinsin))
+# print("Sinsere:{}% Insincere: {}%".format(round(persin,2),round(perinsin,2)))
+print("# Test samples: {:,}({:.3f}% of train samples)".format(len(test_df), len(test_df) / len(train_df)))
+
+# lower
+train_df["question_text"] = train_df["question_text"].apply(lambda x: x.lower())
+test_df["question_text"] = test_df["question_text"].apply(lambda x: x.lower())
+
+# Clean the text
+train_df["question_text"] = train_df["question_text"].progress_apply(lambda x: clean_text(x))
+test_df["question_text"] = test_df["question_text"].apply(lambda x: clean_text(x))
+
+# Clean numbers
+train_df["question_text"] = train_df["question_text"].progress_apply(lambda x: clean_numbers(x))
+test_df["question_text"] = test_df["question_text"].apply(lambda x: clean_numbers(x))
+
+# Clean speelings
+train_df["question_text"] = train_df["question_text"].progress_apply(lambda x: replace_typical_misspell(x))
+test_df["question_text"] = test_df["question_text"].apply(lambda x: replace_typical_misspell(x))
+
+## fill up the missing values
+train_X = train_df["question_text"].fillna("_##_").values
+test_X = test_df["question_text"].fillna("_##_").values
